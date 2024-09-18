10ba25a91e7d10207d660aa7ba51d1e84f73603b
Yohan <yohan.bouali@gmail.com>
2024-09-18 11:15:45 +0200

    fix: remove useless variable from main

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "vpc" {
   vpc_cidr    = "10.0.0.0/16"
   default     = false
   sg_id       = "sg-1234567890abcdef"
-  vpc_id      = "vpc-1234567890abcdef"
   environment = "development"
   subnet_cidr = "10.0.1.0/24"
   subnet_id   = "subnet-1234567890abcdef"
